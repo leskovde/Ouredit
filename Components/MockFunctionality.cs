63a157ce7cbe01b44bb8e65068082a160d64a9ae
Denis <Denis@LAPTOP-J8C4M3I8>
2020-08-16 20:52:02 +0200

    Created a Project Baseline - an ASP. NET Electron project & functionality Class Library

---
+++ Components/MockFunctionality.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Components
+{
+    public class MockFunctionality
+    {
+        public static string SayHello()
+        {
+            return "Hello World!";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# TextEditor
+
+Final lab assignment for the NPRG035 & NRPG038 classes.
