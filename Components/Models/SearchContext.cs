4316869ae5746046ee7756a1d35ad168097810a7
Marketa Sauerova <market.sauer@gmail.com>
2020-09-24 19:22:17 +0200

    #20 Search context model

---
+++ Components/Models/SearchContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Components.Models
+{
+    public class SearchContext
+    {
+        /// <summary>
+        /// Model representing serching text and booleans for serching options.
+        /// </summary>
+        /// <param name="searchText"></param>
+        /// <param name="matchCase"></param>
+        /// <param name="matchWholeWord"></param>
+        /// <param name="useRegex"></param>
+        public SearchContext(
+            string searchText,
+            bool matchCase = false,
+            bool matchWholeWord = false,
+            bool useRegex = false)
+        {
+            SearchText = searchText;
+            MatchCase = matchCase;
+            MatchWholeWord = matchWholeWord;
+            UseRegex = useRegex;
+        }
+
+        public string SearchText { get; }
+
+        public bool MatchCase { get; }
+
+        public bool MatchWholeWord { get; }
+
+        public bool UseRegex { get; }
+    }
+}
